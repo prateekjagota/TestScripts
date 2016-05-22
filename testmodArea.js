4d55f23b2e364fcc08aa0126f488cca3cc2afddd
prateekjagota <Prateek@Prateeks-MacBook-Pro.local>
2016-05-22 20:32:08 +0530

    Added Area functionality

--- testmodArea.js
+++ testmodArea.js
@@ -3,7 +3,7 @@
 #import "config.js"
 
 
-/*test("To test area addition", function(target, app) {
+test("To test area addition", function(target, app) {
      var window = app.mainWindow();
      window.navigationBar().leftButton().vtap();
      window.tableViews()[0].cells()["Areas"].vtap();
@@ -23,7 +23,7 @@
      assertTrue(window.tableViews()[2].cells()[area_name].checkIsValid(), "Area Added Successfully");
      window.navigationBar().leftButton().vtap();
      window.tableViews()[0].cells()["CSRmesh devices"].vtap();
-     });*/
+     });
 
 //Dependent on Area addition test
 
@@ -48,28 +48,54 @@ test("To test area editing", function(target, app) {
      dkeyboard.typeString("\n");
      }
      //Assign Device
-     
-     window.navigationBar().rightButton().vtap();
-     target.delay(3);
+    window.navigationBar().rightButton().vtap();
      assertTrue(window.tableViews()[2].cells()[area_name+"_edited"].isVisible(), "Area Edited Successfully");
 
      window.navigationBar().leftButton().vtap();
      window.tableViews()[0].cells()["CSRmesh devices"].vtap();
      });
-     
 
-//Dependent on Area addition test
+//Dependent on Area addition and editing test
 
-     /*if (window.tableViews()[2].cells()[area_name].checkIsValid()) {
-     test("To test area deletion", function(target, app) {
-          window.tableViews()[2].cells()[area_name].vtap();
-          target.delay(2);
+test("To test assigning device to area", function(target, app) {
+     var window = app.mainWindow();
+     window.navigationBar().leftButton().vtap();
+     window.tableViews()[0].cells()["Areas"].vtap();
+     //loop to get index of settings button
+     cell_length=window.tableViews()[2].cells().length;
+     for(var i =0;i<cell_length;i++) {
+     if (window.tableViews()[2].cells()[i].name() == area_name+"_edited") {
+     window.tableViews()[2].cells()[i].buttons()[0].vtap();
+     }
+     }
+     window.tableViews()[2].groups()["DEVICES"].buttons()["EDIT"].vtap();
+     window.tableViews()[2].cells()[device_name1].vtap();
+     window.navigationBar().rightButton().vtap();
+     //assertEquals(device_name1,window.tableViews()[2].cells()[0].staticTexts()[0].name())
+     target.delay(6);
+     window.navigationBar().rightButton().vtap();
+     window.navigationBar().leftButton().vtap();
+     window.tableViews()[0].cells()["CSRmesh devices"].vtap();
+     });
+
+
+
+//Dependent on Area addition and editing test
+test("To test area deletion", function(target, app) {
+        var window = app.mainWindow();
+        window.navigationBar().leftButton().vtap();
+        window.tableViews()[0].cells()["Areas"].vtap();
+        //loop to get index of settings button
+        cell_length=window.tableViews()[2].cells().length;
+        for(var i =0;i<cell_length;i++) {
+        if (window.tableViews()[2].cells()[i].name() == area_name+"_edited") {
+        window.tableViews()[2].cells()[i].buttons()[0].vtap();
+        }
+        }
           target.frontMostApp().toolbar().buttons()["DELETE"].vtap();
-          target.delay(5);
+          target.delay(3);
           assertNotEquals(area_name,window.tableViews()[2].cells()[area_name].name(),"Area deleted successfully");
           });
-     }
-        else
-          {
-          UIALogger.logFail("Area not created")
-          }*/
+
+
+//Search functionality not supported yet on app
